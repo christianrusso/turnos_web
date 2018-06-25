d7c10760527165a08dee05e32db6a78b3c266d77
Usuario <usuario@MacBook-Pro-de-Usuario.local>
2018-06-24 21:58:51 -0300

    ads

--- src/app/componets/home/home.component.ts
+++ src/app/componets/home/home.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { BaseComponent } from '../../code/base.component';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  extends BaseComponent implements OnInit, AfterViewInit{
 
-  constructor() { }
+  async ngAfterViewInit(): Promise<void> {
+    await this.loadScript('../assets/js/script.js');
+  }
+
+  constructor() {
+    super();
+  }
 
   ngOnInit() {
   }
