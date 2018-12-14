cc493a1b43412d0b345e52a821aa1db4d713ef8f
leandrobono <leandrobono85@gmail.com>
2018-12-14 10:34:04 -0300

    Notifications

--- src/app/clinic/components/editar/editar.component.ts
+++ src/app/clinic/components/editar/editar.component.ts
@@ -17,12 +17,18 @@ export class EditarComponent implements OnInit {
   public isEditing = false;
   public actualPassword;
   public newPassword;
+  public isEditingPersonales = false;
+  public nombre;
+  public apellido;
+  public dni;
+  public emailPersonales;
+  public address;
+  public phone;
 
   constructor(
       private _route: ActivatedRoute,
       private editarService: EditarService
   ) { }
-    public favoritos;
   ngOnInit() {
       this._route.params.subscribe(params => {
           this.userId = params["id"];
@@ -31,7 +37,56 @@ export class EditarComponent implements OnInit {
   }
 
   changeStep(step, element) {
-      //
+      this.step = step;
+      this.removeAllSelected();
+      this.addAllUnselected();
+      if (this.step == 2) {
+          this.getPersonalesData();
+      }
+
+      (document.querySelector('#' + element) as HTMLElement).classList.add('selectedOption');
+  }
+
+  removeAllSelected() {
+      (document.querySelector('#firstStep') as HTMLElement).classList.remove('selectedOption');
+      (document.querySelector('#secondStep') as HTMLElement).classList.remove('selectedOption');
+      (document.querySelector('#thirdStep') as HTMLElement).classList.remove('selectedOption');
+      (document.querySelector('#fourthStep') as HTMLElement).classList.remove('selectedOption');
+      (document.querySelector('#fifthStep') as HTMLElement).classList.remove('selectedOption');
+  }
+  addAllUnselected() {
+      (document.querySelector('#firstStep') as HTMLElement).classList.add('unselectedOption');
+      (document.querySelector('#secondStep') as HTMLElement).classList.add('unselectedOption');
+      (document.querySelector('#thirdStep') as HTMLElement).classList.add('unselectedOption');
+      (document.querySelector('#fourthStep') as HTMLElement).classList.add('unselectedOption');
+      (document.querySelector('#fifthStep') as HTMLElement).classList.add('unselectedOption');
+  }
+
+  getPersonalesData() {
+      this.editarService.getPersonalesData().subscribe(
+          response => {
+              this.nombre          = response.firstName;
+              this.apellido        = response.lastName;
+              this.dni             = response.dni;
+              this.address         = response.address;
+              this.phone           = response.phoneNumber;
+              this.emailPersonales = this.email;
+          },
+          error => {
+              // Manejar errores
+          }
+      );
+  }
+
+  savePersonales() {
+      this.editarService.savePersonalesData(this.nombre, this.apellido, this.address, this.phone, this.dni, this.emailPersonales).subscribe(
+          response => {
+          },
+          error => {
+              // Manejar errores
+          }
+      );
+      this.editStatePersonales();
   }
 
   editState() {
@@ -40,6 +95,14 @@ export class EditarComponent implements OnInit {
       } else if (this.isEditing) {
           this.isEditing = false;
       }
+  }
+
+  editStatePersonales() {
+     if (!this.isEditingPersonales) {
+      this.isEditingPersonales = true;
+     } else if (this.isEditingPersonales) {
+         this.isEditingPersonales = false;
+     }
   }
 
   saveAccount() {
--- src/app/clinic/services/editar.service.ts
+++ src/app/clinic/services/editar.service.ts
@@ -20,6 +20,32 @@ export class EditarService {
     );
   }
 
+  getPersonalesData() {
+    let options = {
+    };
+    return this._http
+        .post(this.url + 'api/Client/GetProfile', options, {
+          headers: this.headers
+        })
+        .map(res => res.json());
+  }
+
+  savePersonalesData(firstName, lastName, address, phone, dni, email) {
+    let options = {
+      "FirstName": firstName,
+      "LastName": lastName,
+      "Address": address,
+      "PhoneNumber": phone,
+      "Dni": dni,
+      "Email": email
+    };
+    return this._http
+        .post(this.url + 'api/Client/Edit', options, {
+          headers: this.headers
+        })
+        .map(res => res.json());
+  }
+
   saveAccountData(username, actualPassword, newPassword) {
     let options = {
       "email": username,
