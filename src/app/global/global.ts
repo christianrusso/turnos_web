c175a3ce2340c2dc8dba147a03515134f2b7912e
leandrobono <leandrobono85@gmail.com>
2019-03-20 11:45:44 -0300

    Url

--- src/app/global/global.ts
+++ src/app/global/global.ts
@@ -1,5 +1,6 @@
 export let global = {
-  url: 'http://localhost:5000/',
+  //url: 'http://localhost:5000/',
+  url: 'https://todoreservas.com.ar:4443/',
   quantityOfResultsToShow: 10,
   source: 2
 };
